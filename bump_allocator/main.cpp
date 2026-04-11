7455c811ce386f9e347aa145adbc3ef97e26e95d
jackeddaniel <siddhant.ugarkar@gmail.com>
2026-04-11 15:19:39 +0530

    back at it

--- bump_allocator/main.cpp
+++ bump_allocator/main.cpp
@@ -1,28 +1,135 @@
+// what are the funcs i need
+// i need a initializer func which will initialize the memory
+// i need a function which return memory address with the requested bytes
+//
+
 #include <iostream>
 #include <unistd.h>
 
 using namespace std;
 
-int MAX_SIZE = 16384;
+void* heap_start;
+void* heap_end;
+void* heap_curr;
 
-void* bump_alloc(void* heap_start, void* heap_end, void* heap_curr, int size) {
-    if (heap_curr < heap_start) {
-        cout<<"Wrong current pointer. Smaller than the start, pls correct"<<endl;
-        return nullptr;
-    }
-    if (heap_curr >= heap_end) {
-        cout<<"You've exhausted you're memory. Sorry"<<endl;
-        return nullptr;
-    }
+void initialize(int bytes) {
+    //assigning the start of the heap
+    heap_start = sbrk(0);
+    heap_curr = heap_start;
 
+    //allocating the required memory
+    sbrk(bytes);
 
+    heap_end = sbrk(0);
 }
 
+void* beg(int bytes) {
+    int size = (char*)heap_end - (char*)heap_curr;
+    if(bytes > size) {
+        cout<<"not enough memory"<<endl;
+        return (void*)0;
+    }
 
+    if(heap_start == heap_end) {
+        cout<<"memory limit reached"<<endl;
+        return (void*)0;
+    }
+
+    void* return_addr = heap_curr;
+
+    cout<<"Here, take you memory boi"<<endl;
+    heap_curr = (char*)heap_curr + x;
+    return return_addr;
+}
 int main() {
-    void* heap_start = sbrk(0);
-    cout<<"Heap starts at :"<<heap_start<<endl;
 
-    sbrk(MAX_SIZE);
-    cout<<"Heap size is :"<<sbrk(0)<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
