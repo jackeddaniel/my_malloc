40d8604bae9a94b5faa3426d4c9d7e99ffa52867
jackeddaniel <siddhant.ugarkar@gmail.com>
2026-04-15 14:30:00 +0530

    fixed the initial size check to use aligned address to compute valide size. also converted all ints to size_t to follow good practice

--- bump_allocator/main.cpp
+++ bump_allocator/main.cpp
@@ -8,7 +8,7 @@ void* heap_start;
 void* heap_end;
 void* heap_curr;
 
-void initialize(int bytes) {
+void initialize(size_t bytes) {
     //assigning the start of the heap
     heap_start = sbrk(0);
     heap_curr = heap_start;
@@ -23,49 +23,31 @@ void reset() {
     heap_curr = heap_start;
 }
 
-void* alloc(int bytes) {
-    int align_val = 16;
+void* alloc(size_t bytes) {
+    size_t align_val = 16;
 
-    int size = (char*)heap_end - (char*)heap_curr;
-    
+    cout<<"addr before alignment: "<<heap_curr<<endl;
+    uintptr_t aligned = (uintptr_t(heap_curr) + align_val - 1) & ~(align_val - 1);    
+    size_t size = (char*)heap_end - (char*)aligned;
+
     if(bytes > size) {
         cout<<"not enough memory"<<endl;
         return (void*)0;
     }
-    void* return_addr = heap_curr;
-    cout<<"addr before alignment: "<<heap_curr<<endl;
-    uintptr_t aligned = (uintptr_t(heap_curr) + align_val - 1) & ~(align_val - 1);    
+    void* return_addr = (void*) aligned;
 
     void* aligned_heap_curr = (void*) aligned;
-
     cout<<"addr after alignment: "<<aligned_heap_curr<<endl;
     heap_curr = (char*)aligned_heap_curr + bytes;
     return return_addr;
 }
 int main() {
     initialize(4096);
-    cout<<"current location of heap allocin: "<<heap_start<<endl;
-    cout<<"current location of heap end: "<<heap_end<<endl;
-    cout<<"current heap curr: "<<heap_curr<<endl;
     alloc(3);
-    cout<<"current heap curr: "<<heap_curr<<endl;
-    alloc(3);
-    cout<<"curren heap curr: "<<heap_curr<<endl;
-    reset();
-    cout<<"heap curr after reset: "<<heap_curr<<endl;
-
-    int* trial = (int*)alloc(sizeof(int));
-    *trial = 4;
-    cout<<*trial<<endl;
-    cout<<trial<<endl;
-    cout<<heap_curr<<endl;
-
-    char* char_trial = (char*)alloc(sizeof(char));
-    *char_trial = 'F';
-    cout<<*char_trial<<endl;
-    cout<<char_trial<<endl;
-    cout<<heap_curr<<endl;
-    alloc(3);
+    alloc(5);
+    alloc(4);
+    alloc(43);
+    alloc(5);
 
 }
 
